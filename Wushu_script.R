fcfc3346e00b1fc83b55553e936c9e764c48dac5
Samuelmontalvo <smontalvo@miners.utep.edu>
2021-06-28 21:19:54 -0600

    a

--- Wushu_script.R
+++ Wushu_script.R
@@ -1,52 +1,129 @@
 library(readxl)
 Wushu <- read_excel("Wushu_Covid_open.xlsx", 
                                sheet = "Jump_Data")
-View(Wushu)
+View(Wushu )
 attach(Wushu)
 library(dplyr)
 
 # Convert Period and Sex from characters to factor variables
 Wushu$Period <- factor(Wushu$Period, 
                        levels = c("Pre","Post","Post+2","Post+4"))
-Wushu$Sex <- as.factor(Wushu$Sexk, levels = c("Male", "Female"))
-
-#Reorder Variables
-Wushu %>%
-  mutate(Period =  factor(Period,
-                          levels = c("Pre","Post","Post+2","Post+4"))) %>%
-  arrange(Period)  
-
-library(rstatix) #package needed for normality and repeated measures anova
-
-#Normality test (Shapiro-Wilk)
-Wushu %>% group_by(Period) %>% shapiro_test(JH)
-
-# Repeated Measures Anova (within subjects) 
-res.aov <- anova_test(data = Wushu, dv = JH, wid = ID, within = Period)
-
-#the Greenhouse-Geisser sphericity correction is automatically applied- 
-#-through the Mauchly's Test for Sphericity 
-get_anova_table(res.aov)
-
-#Post-hoc
-# pairwise comparisons
-pwc <- Wushu %>%
-  pairwise_t_test(JH ~ Period, paired = TRUE,
-    p.adjust.method = "holm")
-pwc
-
-#Plot
-library(ggplot2)
-# Visualization: box plots with p-values
-pwc <- pwc %>% add_xy_position(x = "Period")
-ggplot(Wushu, aes(x = Period,  y = JH, color = Period)) + 
-  geom_boxplot() + theme_classic() +
-  labs(x = "Period", y = "Vertical Jump Height (cm)") +
-  stat_pvalue_manual(pwc,hide.ns = TRUE) 
-
-#Save Plot
-ggsave("VJH.png")
+Wushu$Period <- ordered(Wushu$Period, 
+                       levels = c("Pre","Post","Post+2","Post+4"))
+Wushu$Sex <- as.factor(Wushu$Sex)
 
 # Descriptives
 library(psych)
-Wushu %>% describeBy(Period)
+Wushu%>% describeBy(Period) 
+
+
+#Statistical Analyzes and Plots
+library(rstatix) #needed for normality and Repeated Measures Anova
+#Normality test
+Wushu %>% group_by(Period) %>% shapiro_test(JH)
+
+# Repeated Measures Anova (within subjects) 
+res.aov <- anova_test(data = Wushu, dv = JH, 
+                      wid = ID, within = Period)
+
+# Greenhouse-Geisser sphericity correction is automatically applied- 
+#-through the Mauchly's Test for Sphericity 
+get_anova_table(res.aov)
+
+# Post-hoc pairwise comparisons Holms-Bonferroni correction
+pwc <- Wushu %>%
+  pairwise_t_test(JH ~ Period, paired = TRUE, 
+                  p.adjust.method = "none")
+pwc
+
+#Plots
+library(ggplot2)
+library(ggpubr)
+# Add position for p values in boxplot
+pwc <- pwc %>% add_xy_position(x = "Period")
+# Boxplot of Vertical Jump Height
+p_a <- ggplot(Wushu, aes(x = Period,  y = JH, color = Period)) + 
+  geom_boxplot() + theme_classic() +
+  labs(y = "Vertical Jump Height (cm)", tag = "A)") +
+  stat_pvalue_manual(pwc,size = 2.8, hide.ns = TRUE) 
+#Save Plot
+ggsave("VJH.png")
+
+
+
+# Replication of the RM test and plots for Peak Velocity
+
+#Normality Test
+Wushu %>% group_by(Period) %>% shapiro_test(peak_velocity)
+# Repeated Measures Anova (within subjects) 
+res.aov <- anova_test(data = Wushu, dv = peak_velocity, 
+                      wid = ID, within = Period)
+get_anova_table(res.aov)
+
+#Post-hoc pairwise comparisons
+pwc <- Wushu %>%
+  pairwise_t_test(peak_velocity ~ Period, paired = TRUE, 
+                  p.adjust.method = "none")
+pwc
+
+# Boxplot 
+pwc <- pwc %>% add_xy_position(x = "Period")
+p_b <- ggplot(Wushu, aes(x = Period,  y = peak_velocity, color = Period)) + 
+  geom_boxplot() + theme_classic() +
+  labs(y = "Peak Velocity (m/s)", tag = "B)") +
+  stat_pvalue_manual(pwc,size = 2.8,hide.ns = TRUE) 
+#Save Plot
+ggsave("Peak_Velocity.png")
+
+
+#Replication of Test for Peak Propulsive Power
+
+#Normality Test
+Wushu %>% group_by(Period) %>% shapiro_test(power_max)
+# Repeated Measures Anova (within subjects)
+res.aov <- anova_test(data = Wushu, dv = power_max, 
+                      wid = ID, within = Period)
+get_anova_table(res.aov)
+
+#Post-hoc pairwise comparisons
+pwc <- Wushu %>%
+  pairwise_t_test(power_max ~ Period, paired = TRUE, 
+                  p.adjust.method = "none")
+pwc
+
+# Boxplot 
+pwc <- pwc %>% add_xy_position(x = "Period")
+p_c <- ggplot(Wushu, aes(x = Period,  y = power_max, color = Period)) + 
+  geom_boxplot() + theme_classic() +
+  labs(y = "Peak Propulsive Power (w)", tag = "C)") +
+  stat_pvalue_manual(pwc,size = 2.8,hide.ns = TRUE) 
+#Save Plot
+ggsave("Peak_Propulsive_Power.png")
+
+
+# RSImod
+
+#Normality Test
+Wushu %>% group_by(Period) %>% shapiro_test(RSImodified)
+# Repeated Measures Anova (within subjects)
+res.aov <- anova_test(data = Wushu, dv = RSImodified, 
+                      wid = ID, within = Period)
+get_anova_table(res.aov)
+
+#Post-hoc pairwise comparisons
+pwc <- Wushu %>%
+  pairwise_t_test(RSImodified ~ Period, paired = TRUE, 
+                  p.adjust.method = "none")
+pwc
+
+# Boxplot 
+pwc <- pwc %>% add_xy_position(x = "Period")
+p_d <- ggplot(Wushu, aes(x = Period,  y = RSImodified, color = Period)) + 
+  geom_boxplot() + theme_classic() +
+  labs( y = "RSImod", tag = "D)") +
+  stat_pvalue_manual(pwc,size = 2.8,hide.ns = TRUE) 
+#Save Plot
+ggsave("RSImod.png")
+
+ggarrange(p_a, p_b, p_c, p_d, widths = c(2,2))
+ggsave("all_plots.png")
